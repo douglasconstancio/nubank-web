200822cc71b98e2dc5f6b05503908f216fb487f4
douglasconstancio <douglas.pconstancio@gmail.com>
2022-02-14 21:39:40 -0300

    chore: add src dir and add nextauth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth';
+import GitHubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: "https://github.com/login/oauth/authorize?scope=read:user+user:email",
+    })
+  ],
+  secret: process.env.SECRET,
+  session: {
+    strategy:'jwt'
+  },
+})
