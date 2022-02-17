bdc56bd99a97edb437967f182acf250bac53e00c
douglasconstancio <douglas.pconstancio@gmail.com>
2022-02-17 19:27:09 -0300

    feat: Add shortcuts session and card

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,14 @@
 import { AccountSession } from "../../components/AccountSession";
 import { Header } from "../../components/Header";
+import { ShortcutsSession } from "../../components/ShortcutsSession";
 import { Container } from "./style"
 
 export const Dashboard = () => {
-
   return(
     <Container>
       <Header />
       <AccountSession />
+      <ShortcutsSession />
     </Container>
   )
 }
